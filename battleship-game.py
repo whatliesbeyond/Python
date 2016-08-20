fe80c78165e40a0a09d107430886a306b31f8934
Dewang <dewang.lahariya@gmail.com>
2016-08-20 14:20:42 -0700

    Update and rename battleship.py to battleship-game.py

--- battleship-game.py
+++ battleship-game.py
@@ -1,19 +1,26 @@
+#  Battleship Game with your PC
+#  Instructions: Save this file in any directory, Open your terminal or command prompt, go to that directory. Type "python battleship-game.py"
+#  The game starts. User has to guess the Row (between 0 to 4) and Column (between 0 to 4). Total turns to guess = 4
+#  
+# 
+
+
 from random import randint
 
 board = []
 
 for x in range(5):
-    board.append(["O"] * 5)
+    board.append(["O"] * 5) # using repetition operator to create rows in list format
 
-def print_board(board):
+def print_board(board): # function to print battleship board
     for row in board:
-        print " ".join(row)
+        print " ".join(row) # using join function to print rows in space delimited format
 
 print "Let's play Battleship!"
 print_board(board)
 
 def random_row(board):
-    return randint(0, len(board) - 1)
+    return randint(0, len(board) - 1) 
 
 def random_col(board):
     return randint(0, len(board[0]) - 1)
